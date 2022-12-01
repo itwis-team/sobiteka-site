43852bf26bdc3b3b815fe1518d0d0de6e3bb9472
Anton Romanov <rad250495@yandex.ru>
2022-12-01 11:47:23 +0700

    feat: add Header, MainLayout, Steps markup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ButtonComponent } from './components/button/button.component';
 import { HeaderComponent } from './components/header/header.component';
+import { MainLayoutComponent } from './components/main-layout/main-layout.component';
+import { StepsComponent } from './components/steps/steps.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MainLayoutComponent,
+    ButtonComponent,
+    StepsComponent
   ],
   imports: [
     BrowserModule
